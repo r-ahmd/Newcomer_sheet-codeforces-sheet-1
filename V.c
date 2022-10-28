00b1cbfd1128917e94ed49e0c8d1e706dc0760c5
Tahmed <115088617+r-ahmd@users.noreply.github.com>
2022-10-28 10:26:59 +0600

    Create V.c

---
+++ V.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+   int a, b;
+  char s;
+  scanf("%d %c %d", &a, &s, &b);
+   if(s=='>')
+   {
+      if(a>b)
+         printf("Right\n");
+      else
+         printf("Wrong\n");
+   }
+   else if(s=='<')
+   {
+      if(a<b)
+         printf("Right\n");
+      else
+         printf("Wrong\n");
+   }
+   else if(s=='=')
+   {
+      if(a==b)
+         printf("Right\n");
+      else
+         printf("Wrong\n");
+   }
+    
+    
+    
+   return 0;
+}
