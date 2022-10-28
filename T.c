dfb4f1e392732536a832b223adbcbe954549a374
Tahmed <115088617+r-ahmd@users.noreply.github.com>
2022-10-28 16:20:59 +0600

    Create T.c

---
+++ T.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+  int a, b, c;
+  scanf("%d%d%d", &a, &b, &c);
+  if(a>=b && a>=c)
+  {
+    if(b>=c)
+    {
+      printf("%d\n%d\n%d\n\n", c, b, a);
+      printf("%d\n%d\n%d\n", a, b, c);
+    }
+    else
+    {
+      printf("%d\n%d\n%d\n\n", b, c, a);
+      printf("%d\n%d\n%d\n", a, b, c);
+    }  
+  }
+  else if(b>=a && b>=c)
+  {
+    if(a>=c)
+    {
+      printf("%d\n%d\n%d\n\n", c, a, b);
+      printf("%d\n%d\n%d\n", a, b, c);
+    }
+    else
+    {
+      printf("%d\n%d\n%d\n\n", a, c, b);
+      printf("%d\n%d\n%d\n", a, b, c);
+    }  
+  }
+  else if(c>=a && c>=b)
+  {
+    if(b>=a)
+    {
+      printf("%d\n%d\n%d\n\n", a, b, c);
+      printf("%d\n%d\n%d\n", a, b, c);
+    }
+    else
+    {
+      printf("%d\n%d\n%d\n\n", b, a, c);
+      printf("%d\n%d\n%d\n", a, b, c);
+    }  
+  }
+  
+  
+  return 0;
+}
